7c6a4c37d055226a354f460605fb217a1b7569f3
davidltk123 <davidltk123@gmail.com>
2020-12-02 15:59:34 +0800

    [SpringBoot Practice][David] feat: add get all companies

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -4,14 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+    List<Company> companies = new ArrayList<>();
 
-//    @GetMapping
-//    public List<Company> getAll(){
-//
-//    }
+    @GetMapping
+    public List<Company> getAll(){
+        return companies;
+    }
 }
