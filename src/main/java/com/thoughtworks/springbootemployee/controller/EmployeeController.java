e1e70547854dd98b1a7fc047bcaebced004bee16
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-12-02 15:02:34 +0800

    [spring-boot][Jackie] feat: added create for employees

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.springbootemployee.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +13,12 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAll() {
         return employees;
+    }
+
+    @PostMapping
+    public Employee create(@RequestBody Employee employee) {
+        employees.add(employee);
+        return employee;
     }
 }
 
