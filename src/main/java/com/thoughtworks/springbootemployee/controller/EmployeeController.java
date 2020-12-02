1e06936167cc3bca077b912d30df4ec19c32a294
davidltk123 <davidltk123@gmail.com>
2020-12-02 15:49:54 +0800

    [SpringBoot Practice][David] feat: add findFirst orElse null

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -20,6 +20,7 @@ public class EmployeeController {
 
     @GetMapping("/{employeeId}")
     public Employee getEmployee(@PathVariable Integer employeeId) {
+        //change null to exception
         return employees.stream().filter(employee -> employeeId.equals(employee.getId())).findFirst().orElse(null);
     }
 
