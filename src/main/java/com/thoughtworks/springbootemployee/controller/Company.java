eac601cf7d7d93cbd7b33d88ea547fe88da5c9b6
davidltk123 <davidltk123@gmail.com>
2020-12-02 15:44:31 +0800

    [SpringBoot Practice][David] feat: add company class and controllers

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/Company.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import java.util.List;
+
+public class Company {
+    private String companyName;
+    private Integer employeeNumber;
+    private List<Employee> employees;
+}
