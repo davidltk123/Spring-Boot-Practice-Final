6cd1f57301c37b91df1bd0c179b4dfd111ba5817
davidltk123 <davidltk123@gmail.com>
2020-12-06 14:17:32 +0800

    [Spring Boot Practice][David] fix: fix company integrationtest case for get all and get specific

--- src/test/java/com/thoughtworks/springbootemployee/Intergration/CompanyIntergrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/Intergration/CompanyIntergrationTest.java
@@ -1,78 +1,74 @@
-//package com.thoughtworks.springbootemployee.Intergration;
-//
-//import com.thoughtworks.springbootemployee.Model.Company;
-//import com.thoughtworks.springbootemployee.Model.Employee;
-//import com.thoughtworks.springbootemployee.Repository.CompanyRepository;
-//import com.thoughtworks.springbootemployee.Repository.EmployeeRepository;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class CompanyIntergrationTest {
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Autowired
-//    private CompanyRepository companyRepository;
-//
-//    @Autowired
-//    private EmployeeRepository employeeRepository;
-//
-//    @AfterEach
-//    void tearDown() {
-//        companyRepository.deleteAll();
-//    }
-//
-//    @Test
-//    public void should_return_all_companies_when_get_all_given_all_companies() throws Exception {
-//        //given
-//        List<String> employeeIds = Arrays.asList("1", "2");
-//        Company company = new Company("alibaba", 2, employeeIds);
-//        companyRepository.save(company);
-//        //when
-//        //then
-//        mockMvc.perform(get("/companies"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[0].id").isString())
-//                .andExpect(jsonPath("$[0].companyName").value("alibaba"))
-//                .andExpect(jsonPath("$[0].employeesNumber").value(2))
-//                .andExpect(jsonPath("$[0].employeeIds[0]").value("1"))
-//                .andExpect(jsonPath("$[0].employeeIds[1]").value("2"));
-//    }
-//
-//    @Test
-//    public void should_return_specific_company_when_get_by_id_given_valid_company_id() throws Exception {
-//        //given
-//        List<String> employeeIds = Arrays.asList("1", "2");
-//        Company company = new Company("alibaba", 2, employeeIds);
-//        companyRepository.save(company);
-//        //when
-//        //then
-//        mockMvc.perform(get("/companies/" + company.getId()))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").isString())
-//                .andExpect(jsonPath("$.companyName").value("alibaba"))
-//                .andExpect(jsonPath("$.employeesNumber").value(2))
-//                .andExpect(jsonPath("$.employeeIds[0]").value("1"))
-//                .andExpect(jsonPath("$.employeeIds[1]").value("2"));
-//
-//    }
+package com.thoughtworks.springbootemployee.Intergration;
+
+import com.thoughtworks.springbootemployee.Model.Company;
+import com.thoughtworks.springbootemployee.Model.Employee;
+import com.thoughtworks.springbootemployee.Repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.Repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntergrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    void tearDown() {
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    public void should_return_all_companies_when_get_all_given_all_companies() throws Exception {
+        //given
+        Company company = new Company("alibaba");
+        companyRepository.save(company);
+        //when
+        //then
+        mockMvc.perform(get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isString())
+                .andExpect(jsonPath("$[0].companyName").value("alibaba"))
+                .andExpect(jsonPath("$[0].employeesNumber").value(0))
+                .andExpect(jsonPath("$[0].employees",hasSize(0)));
+    }
+
+    @Test
+    public void should_return_specific_company_when_get_by_id_given_valid_company_id() throws Exception {
+        //given
+        Company company = new Company("alibaba");
+        companyRepository.save(company);
+        //when
+        //then
+        mockMvc.perform(get("/companies/" + company.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.companyName").value("alibaba"))
+                .andExpect(jsonPath("$.employeesNumber").value(0))
+                .andExpect(jsonPath("$.employees", hasSize(0)));
+
+    }
 //
 //    @Test
 //    public void should_return_bad_request_when_get_by_id_given_wrong_format_company_id() throws Exception {
@@ -280,4 +276,4 @@
 //                .content(updateCompanyAsJson))
 //                .andExpect(status().isNotFound());
 //    }
-//}
+}
